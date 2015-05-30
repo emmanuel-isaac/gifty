efb4dbc118bfd0a0f2f56c180cc9d70e9bad625f
andela-eisaac <deejecks@live.com>
2015-05-31 00:42:28 +0100

    Add route to activate account

--- apps/giftyuser/urls.py
+++ apps/giftyuser/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^user/create/$', views.UserCreate.as_view(), name='signup'),
+    url(r'^signup/$', views.UserCreate.as_view(), name='signup'),
+    url(r'^(?P<activation_key>\w+)/activate/', views.register_confirm, name='register_confirm'),
 
 )
