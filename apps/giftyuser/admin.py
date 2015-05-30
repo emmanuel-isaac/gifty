4badac2bb8f5d98563e397c2441cc6e5a1c7ee5c
andela-eisaac <deejecks@live.com>
2015-05-31 00:36:46 +0100

    Register User in admin

--- apps/giftyuser/admin.py
+++ apps/giftyuser/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from apps.giftyuser.models import User
+
 # Register your models here.
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'username', 'email', 'activation_key', 'is_staff', 'is_active', 'phone']
+    list_filter = ['is_superuser', 'is_staff']
+
+admin.site.register(User, UserAdmin)
+
