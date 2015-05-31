d9ceca823eb9d32d58e4b1f3842e7d3132222270
andela-eisaac <deejecks@live.com>
2015-05-31 10:05:51 +0100

    Static files

--- gifty/prod_settings.py
+++ gifty/prod_settings.py
@@ -13,7 +13,8 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
